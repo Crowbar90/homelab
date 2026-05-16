4938a56a7286ff2a7777e724d1220a2269063857
Francesco Venturoli <f.venturoli@gmail.com>
2026-05-16 15:47:37 +0200

    Added sops-nix cache

--- infra/nixos/flake.nix
+++ infra/nixos/flake.nix
@@ -10,6 +10,11 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [ "https://mic92.cachix.org" ];
+    extra-trusted-public-keys = [ "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ=" ];
+  };
+
   outputs =
     { self, nixpkgs, sops-nix, ... }@inputs:
     let
