886629e8af45ccd8e5cc10808c169abbedd23e79
Francesco Venturoli <f.venturoli@gmail.com>
2026-08-19 23:16:46 +0200

    Added Sonarr DBs to Postgresql

--- infra/nixos/hosts/postgresql/postgres.nix
+++ infra/nixos/hosts/postgresql/postgres.nix
@@ -8,15 +8,22 @@
 {
   services.postgresql = {
     enable = true;
+    package = pkgs.postgresql_17;
+    enableTCPIP = true;
 
     ensureDatabases = [
-      "example"
+      "sonarr-main"
+      "sonarr-log"
     ];
 
     ensureUsers = [
       {
-        name = "example";
+        name = "sonarr";
         ensureDBOwnership = true;
+        ensureClauses = {
+          login = true;
+          password = "SCRAM-SHA-256$4096:VbRn9He+emErOwv7OdEFXg==$9iqS8theS9dQJYtwSLjdKKqphi/E0FyRB0854eFr3w4=:o1yGBdTDFn2S5HZG3Bwu8wlMj950W22wWSPeWMt2AJM=";
+        };
       }
     ];
 
@@ -24,6 +31,8 @@
       local   all all trust
       host    all all 127.0.0.1/32 trust
       host    all all ::1/128 trust
+      host    sonarr-main sonarr all scram-sha-256
+      host    sonarr-log sonarr all scram-sha-256
     '';
   };
 
