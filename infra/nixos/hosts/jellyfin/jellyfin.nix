5f396d3d0d5208d78306d2f91a5afcd420dd8879
Francesco Venturoli <f.venturoli@gmail.com>
2026-08-24 17:10:19 +0200

    wip

--- infra/nixos/hosts/jellyfin/jellyfin.nix
+++ infra/nixos/hosts/jellyfin/jellyfin.nix
@@ -1,15 +1,42 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+  ];
+
+  sops = {
+    defaultSopsFile = ../../secrets.yaml;
+    validateSopsFiles = false;
+    age.keyFile = "/var/lib/sops-nix/key.txt";
+
+    secrets."cloudflare-api-token" = {
+      owner = "caddy";
+      group = "caddy";
+      mode = "0400";
+      restartUnits = [ "caddy.service" ];
+    };
+  };
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
   };
 
-  services.caddy.enable = true;
-  services.caddy.virtualHosts."://jellyfin.middleearth.cc".extraConfig = ''
-    reverse_proxy http://localhost:8096
-  '';
+  services.caddy = {
+    enable = true;
+    package = pkgs.caddy.withPlugins {
+      plugins = [ "://github.com" ];
+    };
+    
+    virtualHosts."jellyfin.middleearth.cc".extraConfig = ''
+      reverse_proxy http://localhost:8096
+      
+      tls {
+        dns cloudflare "${config.sops.secrets."cloudflare-api-token".path}"
+      }
+    '';
+  };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
