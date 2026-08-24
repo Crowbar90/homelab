acdca2e4dc94d16f808ad386116efb66d513c800
Francesco Venturoli <f.venturoli@gmail.com>
2026-08-24 16:19:47 +0200

    wip

--- infra/nixos/hosts/jellyfin/configuration.nix
+++ infra/nixos/hosts/jellyfin/configuration.nix
@@ -10,4 +10,16 @@
   networking.hostName = "jellyfin";
 
   system.stateVersion = "25.11";
+
+  boot.supportedFilesystems = ["nfs"];
+
+  fileSystems."/mnt/tower/data" = {
+    device = "192.168.40.2:/mnt/user/data";
+    fsType = "nfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=600"
+    ];
+  };
 }
