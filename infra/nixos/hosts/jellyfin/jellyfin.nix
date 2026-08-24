368e7fb2d0a6add238c737d08a08367bf376ddb3
Francesco Venturoli <f.venturoli@gmail.com>
2026-08-24 17:14:15 +0200

    wip

--- infra/nixos/hosts/jellyfin/jellyfin.nix
+++ infra/nixos/hosts/jellyfin/jellyfin.nix
@@ -27,7 +27,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-      hash = "";
+      hash = "sha256-vNSHU7txQLs0m0UChuszURXjEoMj4r1902+1ei0/DaI=";
     };
     
     virtualHosts."jellyfin.middleearth.cc".extraConfig = ''
